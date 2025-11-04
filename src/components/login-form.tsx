358ca6b00ab437eb06292e20adbb5764e8d90446
Gani <abdulgani.work@gmail.com>
2025-11-04 11:41:32 +0530

    fix: add exclamation mark to welcome message in LoginForm

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -42,7 +42,7 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardTitle className="text-xl">Welcome back!</CardTitle>
           {/* <CardDescription>Login with your Google account</CardDescription> */}
         </CardHeader>
         <CardContent>
